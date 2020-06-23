ab2ed30949eacb054322b284625e4b22922ee52e
Fernando <fernandini50000@gmail.com>
2020-06-23 00:55:36 -0400

    MarketPlace para el boton Sell(Estetico)

--- app/src/main/java/com/example/sportmagazine/MenuTool.java
+++ app/src/main/java/com/example/sportmagazine/MenuTool.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 public class MenuTool extends AppCompatActivity {
-Button noticias,gym;
+
+
+    Button noticias;
+    Button gym;
+    Button marketPlace;
 
 
     @Override
@@ -17,12 +21,22 @@ Button noticias,gym;
         setContentView(R.layout.activity_menutool);
         noticias = (Button)findViewById(R.id.button1);
         gym = (Button)findViewById(R.id.buttonGym);
+        marketPlace = findViewById(R.id.sell);
+
 
         gym.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent activityGym = new Intent(getApplicationContext(),GymActivity.class);
                 startActivity(activityGym);
+            }
+        });
+
+        marketPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activity = new Intent(getApplicationContext(),MarketPlace.class);
+                startActivity(activity);
             }
         });
          noticias.setOnClickListener(
